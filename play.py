bb83e065eff85ef4e4e3063c31db6fed9735c397
usef <usef@mail.com>
2025-11-17 01:48:56 -0800

    navigate path, fix :bug:

--- play.py
+++ play.py
@@ -5,52 +5,67 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import te
 from urllib.parse import unquote
+
+
+# init fastapi
 app=FastAPI()
 
-# app.mount('/static',StaticFiles(directory='static'),name='static')
 
+# init template
 templates=Jinja2Templates(directory='templates')
 
-
+# main page
 @app.get('/',response_class=HTMLResponse)
 async def read(req:Request):
     """Get item in path"""
-    files=[file for file in te.getFiles('/home')][0]
+    files=[file for file in te.getFiles('.')][0]
     return templates.TemplateResponse(
-        request=req,name='index.html',context={"files":files,'path':Path('/home').absolute().as_posix()}
+        request=req,name='index.html',context={"files":files,'path':Path('.').absolute().as_posix()}
         )
 
-
-
+# directory
 @app.get('/dir')
 def dir(req:Request):
+    """Get directory from url and return file, request, path"""
     if req.url.query:
-        path=req.url.query
+        path=unquote(req.url.query)
+        
         if Path(path).exists():
+            
             if Path(path).is_dir():
                 files=[file for file in te.getFiles(path)][0]
+               
                 return templates.TemplateResponse(
                         request=req,name='index.html',context={'files':files,'request':req
                         ,'path':path})
             else:
+                # if request not directory
                 return HTTPException(status_code=406,detail="This file not dir")
+            
         else:
+            # if not exist file
             return HTTPException(status_code=404,detail="Not found")
+        
     else:
+        # if bad url 
         return HTTPException(status_code=400,detail="Bad Request")
     
+
+# show file
 @app.get('/play')
 def play(req:Request,file:str):
-    """"""
+    """
+    Exist file, downlaod or stream 
+    """
     # path=req.url.query
-    print(repr(file))
-    # if file.endswith('.mp4') and file.endswith('.mkv'):
-    #
-    #     return FileResponse(path=file,filename=file.split('/')[-1],media_type="video/mp4")
-    # elif file.endswith('.png') and file.endswith('.png'):
-    #     pass
-    # else:
-    #     return FileResponse(path=file,filename=file.split('/')[-1])
+    # print(repr(file))
+    if file.endswith('.mp4') and file.endswith('.mkv'):
+        return FileResponse(path=file,filename=file.split('/')[-1],media_type="video/mp4")
+    
+    elif file.endswith('.png') and file.endswith('.png'):
+        pass
+    else:
+        return FileResponse(path=file,filename=file.split('/')[-1])
 
 
 
--- te.py
+++ te.py
@@ -1,5 +1,5 @@
-
 from pathlib import Path
+from datetime import datetime
 
 def getFiles(path:str):
     current=Path(path)
@@ -8,10 +8,9 @@ def getFiles(path:str):
         if Path(file).exists():
             if Path(file).is_dir():
                 files['dir']=files.get('dir',[])+[Path(file).absolute().as_posix()]
+                
             else:
                 files['file']=files.get('file',[])+[Path(file).absolute().as_posix()]
 
     yield files
 
-# print([i for i in getFiles('/home')])
-# getFiles('/home')
