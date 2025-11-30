9dc757dbb9b5acca2fc8758d01e0d4ba08029c18
usef <usef@mail.com>
2025-11-30 09:01:52 -0800

    fix bug files

--- te.py
+++ te.py
@@ -65,20 +65,22 @@ def isFile(path:str):
     return Path(path).is_file()
 
 def getFiles(path: str):
-    current = Path(path)
-    files = {'dir': [], 'files': []}
+    if getPermissionFile(path).get('r') and fileExists(path):
+        
+        current = Path(path)
+        files = {'dir': [], 'files': []}
 
-    for file in current.iterdir():
-        if Path(file).exists():
-            if Path(file).is_dir():
-                # files['dir'] = files.get('dir', []) + [Path(file).absolute().as_posix()]
-                files['dir'] = files.get('dir', []) + [
-                    {'path': Path(file).absolute().as_posix(), 'size': getSize(file), 'time_mod': getTimeFile(file),
-                     'perm': getPermissionFile(file)}]
-            else:
-                # files['file'] = files.get('file', []) + [Path(file).absolute().as_posix()]
-                files['files'] = files.get('files', []) + [
-                    {'path': Path(file).absolute().as_posix(), 'size': getSize(file), 'time_mod': getTimeFile(file),
-                     'perm': getPermissionFile(file)}]
+        for file in current.iterdir():
+                if Path(file).is_dir():
+                    # files['dir'] = files.get('dir', []) + [Path(file).absolute().as_posix()]
+                    files['dir'] = files.get('dir', []) + [
+                        {'path': Path(file).absolute().as_posix(), 'size': getSize(file), 'time_mod': getTimeFile(file),
+                        'perm': getPermissionFile(file)}]
+                else:
+                    # files['file'] = files.get('file', []) + [Path(file).absolute().as_posix()]
+                    files['files'] = files.get('files', []) + [
+                        {'path': Path(file).absolute().as_posix(), 'size': getSize(file), 'time_mod': getTimeFile(file),
+                        'perm': getPermissionFile(file)}]
 
-    yield files
+        yield files
+    return {}
